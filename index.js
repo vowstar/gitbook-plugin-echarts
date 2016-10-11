e5c3e72417362a135866c7f9a59c54057f8ac098
Huang Rui <vowstar@gmail.com>
2016-10-11 12:34:51 +0800

    Add echarts

--- index.js
+++ index.js
@@ -1,54 +1,58 @@
-var path    = require('path');
+var path = require('path');
 var phantom = require('phantom');
-var Q       = require('q');
+var Q = require('q');
+
+function processBlock(blk) {
+  var deferred = Q.defer();
+
+  var book = this;
+  var code = blk.body;
+  var config = book.config.get('pluginsConfig.chart', {});
+  //var options = this.book.options.pluginsConfig['chart'];
+  var width = blk.kwargs['width'];
+
+  phantom.create().then(function(ph) {
+    ph.createPage().then(function(page) {
+      var pagePath = path.join(__dirname, 'renderer.html');
+      page.open(pagePath).then(function(status) {
+        var result = page.evaluate(function(code, config, width) {
+          return render(code, config, width);
+        }, code, config, width);
+        ph.exit();
+        deferred.resolve(result);
+      });
+    });
+  });
+
+  return deferred.promise;
+}
 
 module.exports = {
   blocks: {
-    sequence: {
-      process: function(blk) {
-        var deferred = Q.defer();
-
-        var code    = blk.body;
-        var options = this.book.options.pluginsConfig['sequence-diagrams'];
-        var width   = blk.kwargs['width'];
-
-        phantom.create().then(function(ph) {
-          ph.createPage().then(function(page) {
-            var pagePath = path.join(__dirname, 'renderer.html');
-            page.open(pagePath).then(function(status) {
-              var result = page.evaluate(function(code, options, width) {
-                return render(code, options, width);
-              }, code, options, width);
-              ph.exit();
-              deferred.resolve(result);
-            });
-          });
-        });
-
-        return deferred.promise;
-      }
+    chart: {
+      process: processBlock
     }
   },
   hooks: {
     // Init plugin and read config
     "init": function() {
-      if (typeof this.options.pluginsConfig['sequence-diagrams'] === 'undefined') {
-        this.options.pluginsConfig['sequence-diagrams'] = {'theme': 'simple'};
-      }
+      // if (typeof this.options.pluginsConfig['chart'] === 'undefined') {
+      //   this.options.pluginsConfig['chart'] = {};
+      // }
     },
     // Before parsing markdown
     "page:before": function(page) {
-        // Get all code texts
-        flows = page.content.match(/^```sequence((.*\n)+?)?```$/igm);
-        // Begin replace
-        if (flows instanceof Array) {
-          for (var i = 0, len = flows.length; i < len; i++) {
-            page.content = page.content.replace(
-              flows[i], 
-              flows[i].replace(/^```sequence/, '{% sequence %}').replace(/```$/, '{% endsequence %}'));
-          }     
+      // Get all code texts
+      flows = page.content.match(/^```chart((.*\n)+?)?```$/igm);
+      // Begin replace
+      if (flows instanceof Array) {
+        for (var i = 0, len = flows.length; i < len; i++) {
+          page.content = page.content.replace(
+            flows[i],
+            flows[i].replace(/^```chart/, '{% chart %}').replace(/```$/, '{% endchart %}'));
         }
-        return page;
+      }
+      return page;
     }
   }
 };
