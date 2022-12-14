8c863a7f268ba3aa9b59a3207cf4aa0046155715
Huang Rui <vowstar@gmail.com>
2022-12-14 22:11:39 +0800

    index.js: change fallback svg width to 600px
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function processBlock(blk) {
             JSON.parse(code);
         } catch (e) {
             console.error("Invalid JSON: " + code);
-            deferred.resolve('<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><text x="10" y="100" font-size="60" text-anchor="left">Invalid JSON format! Please modify your JSON input.</text></svg>');
+            deferred.resolve('<svg version="1.1" width="600" height="200" xmlns="http://www.w3.org/2000/svg"><text x="10" y="100" font-size="60" text-anchor="left">Invalid JSON format! Please modify your JSON input.</text></svg>');
             return deferred.promise;
         }
     }
